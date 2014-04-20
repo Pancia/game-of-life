ee31aec8955d2fb9902adc403ae201cffee9c979
pancia <nsa.anthony@gmail.com>
2014-04-20 02:30:55 -0700

    Added GameBoard() and GameCreature()

--- gameoflife.py
+++ gameoflife.py
@@ -1,26 +1,28 @@
 import time
 
+#consider switching to outputting to GUI for learning purposes
 def main():
 	test = str(raw_input("test_run?[Y/n] "))
 	if test in "Yy ":
-		play_game_of_life(6, 10, .5, True)
+		play_game_of_life(6, 13, .3, True)
 	else:
-		play_game_of_life(
-			int(raw_input("max_size? ")),
-			int(raw_input("num_iters? ")),
-			float(raw_input("sleep_time? ")),
-			False
-		)
+		try:
+			play_game_of_life(
+				int(raw_input("max_size? ")),
+				int(raw_input("num_iters? ")),
+				float(raw_input("sleep_time? ")),
+				False)
+		except ValueError, ve:
+			print "\tERROR: "+str(ve)
+			print "\tTRY AGAIN"
+			main()
 
-#upgrade game_board to class
-#with fields, such as game_board, num_live_cells, etc,
-#and functions, such as update_board(), make_cell_alive(), etc.
+#add self.num_live_cells 
 def play_game_of_life(max_size, num_iters, sleep_time, is_test_run):
-	game_board = [[' ' for i in range(max_size)] for j in range(max_size)]
+	game_board = GameBoard(max_size)
 	if not is_test_run:
-		should_add_glider = str(raw_input("add_glider?[Y/n] ")).lower()
-		if should_add_glider in "y ":
-			add_glider(game_board, max_size);
+		if str(raw_input("add_glider?[Y/n] ")) in "yY":
+			game_board.add_creature(GameCreature("glider"), 0, 0)
 		else:
 			print "enter \"stop\" or \"done\" when finished"
 			while True:
@@ -30,77 +32,110 @@ def play_game_of_life(max_size, num_iters, sleep_time, is_test_run):
 				j = raw_input("j? ")
 				if j in ["stop", "done"]:
 					break;
-				make_alive(game_board, max_size, int(i), int(j))
+				game_board.set_cell_to(int(i), int(j), True)
 	else:
-		add_glider(game_board, max_size)
+		game_board.add_creature(GameCreature("glider"), 0, 0)
 
-	print_game_of_life(game_board, max_size)
+	game_board.print_game_board()
 	for q in range(num_iters):
-		game_board = update_game_board(game_board, max_size)
-		print_game_of_life(game_board, max_size)
+		game_board.update_game_board()
+		game_board.print_game_board()
 		time.sleep(sleep_time)
-	end = raw_input("end")
+	end = raw_input("end?[Y/n] ")
+	if end not in "yY":
+		main()
 
-#refactor to class/subclass
-#and add starting location, rather than set board position
-def add_glider(game_board, max_size): #ADD: (i, j):
-	for i, j in [[0,0], [1,1], [1,2], [2,1], [0,2]]:
-		make_alive(game_board, max_size, i, j)
+class GameBoard():
+	live_cell = 'x'
+	dead_cell = ' '
 
-def update_game_board(game_board, max_size):
-	new_game_board = [row[:] for row in game_board]
-	for i in range(max_size):
-		for j in range(max_size):
-			num_neighbors = get_neighbor_count(game_board, max_size, i, j)
-			if game_board[i][j] == 'x':
-				if num_neighbors < 2 or num_neighbors > 3:
-					new_game_board[i][j] = ' '
+	def __init__(self, max_size):
+		self.max_size = max_size
+		self.game_board = [[self.dead_cell for i in range(max_size)] for j in range(max_size)]
+		self.num_live_cells = 0
+
+	def add_creature(self, creature, x, y): 
+		for i, j in creature.get_cells():
+			self.set_cell_to(x+i, y+j, True)
+
+	def update_game_board(self):
+		if self.num_live_cells == 0:
+			return
+		new_game_board = GameBoard(self.max_size)
+		new_game_board.num_live_cells = self.num_live_cells
+		new_game_board.game_board = [row[:] for row in self.game_board]
+
+		for i in range(self.max_size):
+			for j in range(self.max_size):
+				num_neighbors = self.get_neighbor_count(i, j)
+				if self.game_board[i][j] == self.live_cell:
+					if num_neighbors < 2 or num_neighbors > 3:
+						new_game_board.set_cell_to(i, j, False)
+				else:
+					if num_neighbors == 3:
+						new_game_board.set_cell_to(i, j, True)
+
+		self.game_board = new_game_board.game_board
+
+	def get_neighbor_count(self, i, j):
+		count = 0
+		for x in range(-1, 2):
+			for y in range(-1, 2):
+				if x==0 and y==0:
+					continue
+				if i+x < 0: 
+					i_x = self.max_size-1
+				elif i+x > self.max_size-1:
+					i_x = 0
+				else:
+					i_x = i+x
+				if j+y < 0:
+					j_y = self.max_size-1
+				elif j+y > self.max_size-1:
+					j_y = 0
+				else:
+					j_y = j+y
+				if self.game_board[i_x][j_y] == self.live_cell:
+					count += 1
+		return count
+
+	def print_game_board(self):
+		print ""
+		for i in range(self.max_size):
+			buf = ""
+			sep = ""
+			for j in range(self.max_size):
+				buf += self.game_board[i][j] + "|"
+				sep += "--"
+			print buf
+			print sep	
+		print ""
+
+	def set_cell_to(self, i, j, alive):
+		try:
+			if alive:
+				self.game_board[i][j] = self.live_cell
+				self.num_live_cells += 1
 			else:
-				if num_neighbors == 3:
-					new_game_board[i][j] = 'x'
-	return new_game_board
+				self.game_board[i][j] = self.dead_cell
+				self.num_live_cells -= 1
 
-def get_neighbor_count(game_board, max_size, i, j):
-	count = 0
-	for x in range(-1, 2):
-		for y in range(-1, 2):
-			if x==0 and y==0:
-				continue
-			if i+x < 0: 
-				i_x = max_size-1
-			elif i+x > max_size-1:
-				i_x = 0
-			else:
-				i_x = i+x
-			if j+y < 0:
-				j_y = max_size-1
-			elif j+y > max_size-1:
-				j_y = 0
-			else:
-				j_y = j+y
-			if game_board[i_x][j_y] == 'x':
-				count += 1
-	return count
+		except IndexError, e:
+			print str(e) + "; max_size is " + str(max_size)
 
-def print_game_of_life(game_board, max_size):
-	print ""
-	for i in range(max_size):
-		buf = ""
-		sep = ""
-		for j in range(max_size):
-			buf += game_board[i][j] + "|"
-			sep += "--"
-		print buf
-		print sep	
-	print ""
+#how to implement prompting and saving (persistently) user created creatures?
+class GameCreature():
+	glider_cells = [[0,0], [1,1], [1,2], [2,1], [0,2]]
 
-#either add method or add to this
-#func to make_dead if already alive
-def make_alive(game_board, max_size, i, j):
-	try:
-		game_board[i][j] = 'x'
-	except IndexError, e:
-		print str(e) + "; max_size is " + str(max_size)
+	def __init__(self, creature_type):
+		self.creature_type = creature_type
+		if self.creature_type == "glider":
+			self.cells = self.glider_cells
+		else: #ie default
+			self.cells = self.glider_cells
+
+	def get_cells(self):
+		return self.cells
 
 if __name__ == "__main__":
 	main()
